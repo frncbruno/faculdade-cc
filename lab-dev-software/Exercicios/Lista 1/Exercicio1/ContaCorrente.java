4f1535e0946cfc384d3f8ce2ad2c4fd919b243d6
Bruno Tubino Franco <tubino.bruno@ufn.edu.br>
2026-07-29 11:09:06 -0300

    Create ContaCorrente.java

---
+++ lab-dev-software/Exercicios/Lista 1/Exercicio1/ContaCorrente.java
@@ -0,0 +1,36 @@
+package com.mycompany.projetoconta;
+
+/**
+ *
+ * @author laboratorio
+ */
+public class ContaCorrente {
+    private float saldo;
+
+    public float getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(float saldo) {
+        this.saldo = saldo;
+    }
+    
+    public void definirSaldoInicial(float valor){
+        saldo = valor;
+    }
+    
+    public void depositar(float valor){
+        saldo = saldo + valor;
+    }
+    
+    public boolean sacar(float valor){
+        if(saldo >= valor){
+           saldo -= valor;
+           System.out.println("Saque feito com sucesso.");
+           return true;
+        } else {
+           System.out.println("Não é possível sacar, saldo menor que valor pedido.");
+           return false;
+        }
+    }
+}
