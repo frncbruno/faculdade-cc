ab35d14226741d537816a48d7db6eee6a9ce8188
Bruno Tubino Franco <tubino.bruno@ufn.edu.br>
2026-07-29 11:37:55 -0300

    Create FormaGeometrica.java

---
+++ lab-dev-software/Exercicios/Lista 1/Exercicio2/FormaGeometrica.java
@@ -0,0 +1,25 @@
+public class FormaGeometrica {
+    private float lado;
+    protected float area;
+
+    public float getLado() {
+        return lado;
+    }
+
+    public void setLado(float lado) {
+        this.lado = lado;
+    }
+
+    public float getArea() {
+        return area;
+    }
+
+    public void setArea(float area) {
+        this.area = area;
+    }
+    
+    public float calcularArea(){
+        area = lado * lado;
+        return area;
+    }
+}
