d395e3ce19e52b3c4038c7f486b37a007db757bc
Bruno Tubino Franco <tubino.bruno@ufn.edu.br>
2026-09-07 16:57:44 -0300

    Add files via upload

---
+++ lab-dev-software/Exercicios/Lista 7/Projeto/src/projeto/Aluno.java
@@ -0,0 +1,170 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package projeto;
+
+/**
+ *
+ * @author laboratorio
+ */
+public class Aluno {
+    private String nome;
+    private String dataNascimento;
+    private String sexo;
+    private int matricula;
+    private String curso;
+    private String cpf;
+    private String rua;
+    private String numero;
+    private String bairro;
+    private String cidade;
+    private String cep;
+    private String estado;
+    private String telefone;
+
+    public Aluno(String nome, String dataNascimento, String sexo,
+                 int matricula, String curso, String cpf,
+                 String rua, String numero, String bairro,
+                 String cidade, String cep, String estado,
+                 String telefone) {
+
+        this.nome = nome;
+        this.dataNascimento = dataNascimento;
+        this.sexo = sexo;
+        this.matricula = matricula;
+        this.curso = curso;
+        this.cpf = cpf;
+        this.rua = rua;
+        this.numero = numero;
+        this.bairro = bairro;
+        this.cidade = cidade;
+        this.cep = cep;
+        this.estado = estado;
+        this.telefone = telefone;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    
+
+    @Override
+    public String toString() {
+        return nome + ";" +
+               dataNascimento + ";" +
+               sexo + ";" +
+               matricula + ";" +
+               curso + ";" +
+               cpf + ";" +
+               rua + ";" +
+               numero + ";" +
+               bairro + ";" +
+               cidade + ";" +
+               cep + ";" +
+               estado + ";" +
+               telefone;
+    }
+}
+
